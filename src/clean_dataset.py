428c19f3f56795571af8f1f01f9fb685c4c38753
lukas <82029186+42lukas@users.noreply.github.com>
2025-11-26 11:35:48 +0100

    added cleaning script for sentiment140

---
+++ src/clean_dataset.py
@@ -0,0 +1,23 @@
+# src/clean_dataset.py
+
+# this file is to clean the Sentiment140 dataset after downloading
+# it removes unnecessary columns
+
+import pandas as pd
+
+df = pd.read_csv("/Users/lukaskarsten/Desktop/Informatik/Repos/fk-svm-sentiment-analysis/data/training.1600000.processed.noemoticon.csv",
+                names=['polarity', 'id', 'date', 'query', 'user', 'text'],
+                encoding='latin-1')
+print(df.head()) # print the first 5 rows to see the structure
+print(df.polarity.value_counts()) # check the distribution of classes
+
+# keep only the necessary columns: polarity and text
+df = df.drop(columns=['id', 'date', 'query', 'user'])
+print(df.head())
+
+# switch polarity values from 0,4 to 0,1
+df.polarity = df.polarity.replace({0: 0, 4: 1})
+print(df.polarity.value_counts())
+
+# save the cleaned dataset
+df.to_csv("data/sentiment140-subset.csv", index=False)
